b784528decb0cf47abf06382b0234827670ec06e
fascinated <liam@fascinated.cc>
2026-09-09 22:26:49 +0100

    oops

--- src/feature/index.ts
+++ src/feature/index.ts
@@ -1,11 +1,21 @@
+import { discordClient } from "..";
+import type Feature from "./feature";
 import InvitesFeature from "./invites";
 import SocialFeature from "./social";
 import StatsFeature from "./stats";
 
 export default class FeatureManager {
+  private static FEATURES: Feature[] = [];
+
   constructor() {
-    new StatsFeature();
-    new SocialFeature();
-    new InvitesFeature();
+    FeatureManager.registerFeature(new StatsFeature());
+    FeatureManager.registerFeature(new SocialFeature());
+    FeatureManager.registerFeature(new InvitesFeature());
+
+    FeatureManager.FEATURES.forEach((feature) => feature.registerHandlers(discordClient));
+  }
+
+  public static registerFeature(feature: Feature): void {
+    this.FEATURES.push(feature);
   }
 }
