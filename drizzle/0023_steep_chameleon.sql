60937d36c3b368fccbf3d3980dcc1096fca0b36d
fascinated <liam@fascinated.cc>
2026-09-10 17:57:59 +0100

    really fix??

--- drizzle/0023_steep_chameleon.sql
+++ drizzle/0023_steep_chameleon.sql
@@ -1,2 +1,3 @@
+ALTER TABLE "level_configs" ALTER COLUMN "ignored_channel_ids" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "level_configs" ALTER COLUMN "ignored_channel_ids" SET DATA TYPE jsonb USING "ignored_channel_ids"::jsonb;--> statement-breakpoint
 ALTER TABLE "level_configs" ALTER COLUMN "ignored_channel_ids" SET DEFAULT '[]'::jsonb;
