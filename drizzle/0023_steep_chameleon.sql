9fdb68b291ec1dd4e75766344e4084ad71edfc9b
fascinated <liam@fascinated.cc>
2026-09-10 17:55:24 +0100

    fix migraion

--- drizzle/0023_steep_chameleon.sql
+++ drizzle/0023_steep_chameleon.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "level_configs" ALTER COLUMN "ignored_channel_ids" SET DATA TYPE jsonb;--> statement-breakpoint
+ALTER TABLE "level_configs" ALTER COLUMN "ignored_channel_ids" SET DATA TYPE jsonb USING "ignored_channel_ids"::jsonb;--> statement-breakpoint
 ALTER TABLE "level_configs" ALTER COLUMN "ignored_channel_ids" SET DEFAULT '[]'::jsonb;
